52eddd1b6bd127ed95f590c497ba0a813e9b1ba5
User3 <User3@gmail.com>
2024-12-09 11:11:49 +0900

    Add OnBnClickedMsgBtn() function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // JavaScript to change the text of the paragraph when button is clicked
-document.getElementById('changeTextButton').addEventListener('click', function() {
-    const content = document.querySelector('.content p');
-    content.textContent = 'The text has been changed! Now, try committing this change in Git!';
+document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
+    alert("Button cliked!");
 });
