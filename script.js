a6fa7ebcfd06a92a46bc00dd4e2bffef0c1f45c5
User3 <User3@gmail.com>
2024-12-09 11:15:09 +0900

    Add listbox and modify OnBnClickedMsgBtn function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // JavaScript to change the text of the paragraph when button is clicked
 document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
-    alert("Button cliked!");
+    const content = document.querySelector('.content p');
+    content.innerHTML += '<br>Button clicked!'; // <br> 태그로 줄 바꿈을 추가
 });
