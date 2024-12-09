5ec91cc7ef5f83d9b0d65ced77b7c81a7c9ff540
User3 <User3@gmail.com>
2024-12-09 15:01:46 +0900

    Modify OnBnClickedMsgBtn to print # of clicks

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // JavaScript to change the text of the paragraph when button is clicked
+var cnt = 0;
 document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
     const content = document.querySelector('.content p');
     content.innerHTML += '<br>Button clicked!'; // <br> 태그로 줄 바꿈을 추가
+    content.innerHTML += ++cnt; // <br> 태그로 줄 바꿈을 추가
 });
 
 document.getElementById('OnBnClickedResetBtn').addEventListener('click', function() {
     const content = document.querySelector('.content p');
     content.textContent = 'This is a simple web page to practice Git and GitHub.';
+    cnt = 0;
     alert("Reset content!");
 });
